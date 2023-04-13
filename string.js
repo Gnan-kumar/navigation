71ef4113bdc04840228daac1c78c701f834b257b
Gnan Kumar <gnankumar@technqtechnologies.com>
2023-04-13 09:35:17 +0530

    navs

---
+++ string.js
@@ -0,0 +1,44 @@
+const str = 'Life, the universe and everything. Answer:';
+
+console.log(`${str} ${str.length}`);
+// Expected output: "Life, the universe and
+const a = 'réservé'; // With accents, lowercase
+const b = 'RESERVE'; // No accents, uppercase
+
+console.log(a.localeCompare(b));
+// Expected output: 1
+console.log(a.localeCompare(b, 'en', { sensitivity: 'base' }));
+// Expected output: 0
+const animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
+
+console.log(animals.lastIndexOf('Dodo'));
+// Expected output: 3
+
+console.log(animals.lastIndexOf('Tiger'));
+// Expected output: 1
+const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+const regex = /[A-Z]/g;
+const found = paragraph.match(regex);
+
+console.log(found);
+// Expected output: Array ["T", "I"]
+const str1 = 'Breaded Mushrooms';
+
+console.log(str1.padEnd(25, '.'));
+// Expected output: "Breaded Mushrooms........"
+
+const str2 = '200';
+
+console.log(str2.padEnd(5));
+// Expected output: "200  "
+const str3 = '5';
+
+console.log(str3.padStart(2, '0'));
+// Expected output: "05"
+
+const fullNumber = '2034399002125581';
+const last4Digits = fullNumber.slice(-4);
+const maskedNumber = last4Digits.padStart(fullNumber.length, '*');
+
+console.log(maskedNumber);
+// Expected output: "******5581"
